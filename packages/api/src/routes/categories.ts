73480a574168c15f1360aaa367abde7b3a731fbf
koralle <koralle@koralle-mgmg.com>
2026-01-04 19:46:02 +0900

    feat: wip

--- packages/api/src/routes/categories.ts
+++ packages/api/src/routes/categories.ts
@@ -3,11 +3,11 @@ import { Hono } from "hono";
 import { categories } from "../db";
 
 export const categoriesApp = new Hono<{ Bindings: { DB: D1Database } }>().get("/", async (c) => {
-  const db = drizzle(c.env.DB)
+  const db = drizzle(c.env.DB);
 
-  const allCategories = await db.select().from(categories).all()
+  const allCategories = await db.select().from(categories).all();
 
   return c.json({
-    categories: allCategories
+    categories: allCategories,
   });
 });
